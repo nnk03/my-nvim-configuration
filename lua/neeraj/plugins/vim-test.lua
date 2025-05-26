6812cfddcd93e827c9f43e52ef4591bd36d4908c
Neeraj Krishna N <neerajnjps03@gmail.com>
2025-05-26 12:26:23 +0530

    Updated Config

--- lua/neeraj/plugins/vim-test.lua
+++ lua/neeraj/plugins/vim-test.lua
@@ -5,7 +5,7 @@ return {
 	},
 	vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", { desc = "Test Nearest" }),
 	vim.keymap.set("n", "<leader>T", ":TestFile<CR>", { desc = "Test File" }),
-	vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", { desc = "Test Suite" }),
+	-- vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", { desc = "Test Suite" }),
 	vim.keymap.set("n", "<leader>l", ":TestLast<CR>", { desc = "Test Last" }),
 	vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", { desc = "Test Visit" }),
 
